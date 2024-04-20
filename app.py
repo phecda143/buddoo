8adb49055df94092f045792a2f9241c750762b4f
user <agnessaayrapetova@gmail.com>
2024-04-20 21:19:42 +0300

    commit 1 add registration

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def registration():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+
+        new_user = User(username=username, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/success')
+
+    return render_template('index.html')
+
+
+@app.route('/success')
+def success():
+    return 'Registration Successful!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
